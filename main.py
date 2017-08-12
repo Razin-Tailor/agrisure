f7d55c9533493b1d45a9856f232497fd168c796a
Razin Tailor <r42intailor@gmail.com>
2017-08-12 21:21:42 +0530

    test3

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def graph(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
     title = {"text": 'My Title'}
     xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
     yAxis = {"title": {"text": 'yAxis Label'}}
-    return render_template('chart.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+    return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
   # /* Endpoint to greet and add a new visitor to database.
 # * Send a POST request to localhost:8080/api/visitors with body
 # * {
