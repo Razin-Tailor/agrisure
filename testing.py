628cf04d48d5d78c3f4d939db906d576559472b8
Razin Tailor <r42intailor@gmail.com>
2017-08-13 09:00:00 +0530

    trblshoot

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
