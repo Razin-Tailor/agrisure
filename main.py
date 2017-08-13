d28255ad10cae0f01bf1cee8a6f006e4e1ab7218
Razin Tailor <r42intailor@gmail.com>
2017-08-13 08:07:25 +0530

    cleaning

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 from fuzzywuzzy import fuzz
-from crab import *
+# from crab import *
 # import matplotlib.pyplot as plt
 
 
@@ -107,6 +107,50 @@ def rec_rain():
         return jsonify({'suggestion' : recommendations})
     else:
         return jsonify({'status': 'failure'})
+
+def dynamic_model(season, param):
+    df = df[df['season'] == season]
+    df.drop('season', axis=1, inplace=True)
+    if df['crop'].isnull().sum() > 0:
+        df = df.dropna(axis = 0, subset = ['crop'])
+
+    # print("After processing-------",len(df['crop'].unique()))
+    crops = (df.groupby(by = ['crop'])[param].sum().reset_index().rename(columns = {param: 'total_crop_value'})[['crop', 'total_crop_value']])
+
+    # print(crops.head())
+
+    df_with_crops = df.merge(crops, left_on = 'crop', right_on = 'crop', how = 'left')
+    # print(df_with_crops.head())
+
+    # print("Total crop value", crops['total_crop_value'].describe())
+    print crops['total_crop_value'].quantile(np.arange(.9, 1, .01))
+
+    popularity_threshold = 310977
+    df_popular_crops = df_with_crops.query('total_crop_value >= @popularity_threshold')
+    # print("df_of_popular_crop", df_popular_crops.head())
+    in_data = df_popular_crops
+    # print("In-data---", in_data.head(), len(in_data['crop'].unique()))
+    if not in_data[in_data.duplicated(['location', 'crop'])].empty:
+        initial_rows = in_data.shape[0]
+
+        print 'Initial dataframe shape {0}'.format(in_data.shape)
+        in_data = in_data.drop_duplicates(['location', 'crop'])
+        current_rows = in_data.shape[0]
+        print 'New dataframe shape {0}'.format(in_data.shape)
+        print 'Removed {0} rows'.format(initial_rows - current_rows)
+    else:
+        print("consistent data")
+    global wide_df
+    wide_df = in_data.pivot(index = 'crop', columns = 'location', values = param).fillna(0)
+    # print(wide_df.head(), wide_df.shape)
+    # print((wide_df.columns.tolist()))
+    wide_df_sparse = csr_matrix(wide_df.values)
+
+    #### Fitting Model ####
+
+    
+    model_knn.fit(wide_df_sparse)
+
 
 if __name__ == '__main__':
     # prepare_model()
